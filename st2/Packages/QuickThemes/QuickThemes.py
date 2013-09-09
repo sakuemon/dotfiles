274b96c8ccffc9479a20ffeb36b25dfed9d2efba
Tsuyoshi MIYAMOTO <sakuemon@gmail.com>
2013-09-09 17:19:10 +0900

    modify some st2 packages.

--- st2/Packages/QuickThemes/QuickThemes.py
+++ st2/Packages/QuickThemes/QuickThemes.py
@@ -1,27 +1,39 @@
 import sublime
 import sublime_plugin
+import re
 
 
 class QuickThemesCommand(sublime_plugin.WindowCommand):
-    def run(self, action):
+    def theme_name_status_message(self, full_name):
+        try:
+            match = re.search("([^/]+).tmTheme|.sublime-theme$", full_name)
+            theme_name = match.group(1)
 
-        qt_settings = sublime.load_settings(__name__ + '.sublime-settings')
+            if theme_name:
+                sublime.status_message(theme_name)
+        except:
+            pass
+
+    def get_mismatch(self, a, b):
+            """ Return the difference between two dicts. """
+            diff = set(a).difference(set(b))
+            return dict((key, value) for (key, value) in diff)
+
+    def run(self, action):
+        qt_settings = sublime.load_settings('QuickThemes.sublime-settings')
         full_settings = sublime.load_settings("Base File.sublime-settings")
         relevant_settings = {}
 
         qt_defaults = qt_settings.get("quick_themes_defaults")  # dict
         qt_themes = qt_settings.get("quick_themes")  # list
-        qt_selection = int(qt_settings.get("quick_themes_selection"))  # int
+        qt_selection = int(qt_settings.get("quick_themes_selection", 0))  # int
 
         for option in qt_defaults:
             relevant_settings[option] = full_settings.get(option)
 
-        def get_mismatch(a, b):
-            """ Return the difference between two dicts. """
-            diff = set(a).difference(set(b))
-            return dict((key, value) for (key, value) in diff)
+        mismatch = self.get_mismatch(
+            qt_themes[qt_selection], relevant_settings)
 
-        mismatch = get_mismatch(qt_themes[qt_selection], relevant_settings)
         if len(mismatch) > 0:
             """ There is a mismatch between the selected quicktheme
                 and the current base theme settings. Check to see whether
@@ -30,7 +42,7 @@ class QuickThemesCommand(sublime_plugin.WindowCommand):
             for theme in qt_themes:
                 test = qt_defaults
                 test.update(theme)
-                if get_mismatch(test, relevant_settings).len == 0:
+                if self.get_mismatch(test, relevant_settings).len == 0:
                     match = True
                     """ Found a match, so update the selection setting. """
                     qt_selection = qt_themes.index(theme)
@@ -51,14 +63,14 @@ class QuickThemesCommand(sublime_plugin.WindowCommand):
                 qt_selection = len(qt_themes) - 1
 
         writeable_settings = dict(qt_defaults, **qt_themes[qt_selection])
+
         for option in writeable_settings:
             full_settings.set(option, writeable_settings[option])
 
         qt_settings.set("quick_themes_selection", int(qt_selection))
         qt_settings.set("quick_themes", qt_themes)
 
-        sublime.save_settings(__name__ + '.sublime-settings')
+        sublime.save_settings('QuickThemes.sublime-settings')
         sublime.save_settings("Base File.sublime-settings")
 
-        sublime.status_message("QuickThemes: theme index is now "
-            + str(qt_selection))
+        self.theme_name_status_message(writeable_settings["color_scheme"])
