f439505d37a3fd5bb0f54ae3af355073c38718f3
Tsuyoshi MIYAMOTO <sakuemon@gmail.com>
2020-01-19 20:01:01 +0900

    clean and add fzf configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,15 +2,23 @@ set fish_key_bindings fish_vi_key_bindings
 
 set -x XDG_CONFIG_HOME $HOME/.config
 
-alias rm='rm -i'
-
-abbr --add view nvim -R
-
 set LANG ja_JP.UTF-8
 set PATH $HOME/go/bin $PATH
+
+# common
+alias rm='rm -i'
+
+# vim, nvim
+alias vim='nvim'
+abbr --add view nvim -R
 
 # nvm
 set -x NVM_DIR "$HOME/.nvm"
 
 # go
 set -x GO111MODULE on
+
+# fzf
+set -x FZF_DEFAULT_OPTS "--no-height --no-reverse"
+set -x FZF_TMUX 1
+
