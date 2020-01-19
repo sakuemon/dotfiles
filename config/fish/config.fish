be67ef7db51ed155575d28b2b90227c7d2f98556
Tsuyoshi MIYAMOTO <sakuemon@gmail.com>
2020-01-19 23:25:50 +0900

    change fzf configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,12 +4,14 @@ set -x XDG_CONFIG_HOME $HOME/.config
 
 set LANG ja_JP.UTF-8
 set PATH $HOME/go/bin $PATH
+set EDITOR vim
 
 # common
 alias rm='rm -i'
 
 # vim, nvim
 alias vim='nvim'
+alias vi='nvim'
 abbr --add view nvim -R
 
 # nvm
@@ -21,4 +23,5 @@ set -x GO111MODULE on
 # fzf
 set -x FZF_DEFAULT_OPTS "--no-height --no-reverse"
 set -x FZF_TMUX 1
-
+set -U FZF_ENABLE_OPEN_PREVIEW 1 
+set -U FZF_FIND_FILE_OPTS "--preview 'cat {}'" 
