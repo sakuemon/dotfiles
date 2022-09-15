ce4870e3a9b4e95a28aa1dd9d36a33218d8326c5
Tsuyoshi MIYAMOTO <sakuemon@gmail.com>
2022-09-15 23:14:34 +0900

    add go imports

---
+++ config/nvim.lua/after/ftplugin/go.lua
@@ -0,0 +1,21 @@
+local function go_org_imports(wait_ms)
+	return function()
+		local params = vim.lsp.util.make_range_params()
+		params.context = {only = {"source.organizeImports"}}
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+		for cid, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+					vim.lsp.util.apply_workspace_edit(r.edit, enc)
+				end
+			end
+		end
+	end
+end
+
+vim.api.nvim_create_autocmd('BufWritePre',{
+	pattern = {'*.go'},
+	callback = go_org_imports()
+})
+-- autocmd BufWritePre *.go lua go_org_imports()
