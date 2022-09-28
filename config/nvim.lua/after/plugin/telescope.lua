7712a25e069de30b096384ce31e9e41cb670faa7
Tsuyoshi MIYAMOTO <sakuemon@gmail.com>
2022-09-28 22:04:52 +0900

    set layout

--- config/nvim.lua/after/plugin/telescope.lua
+++ config/nvim.lua/after/plugin/telescope.lua
@@ -3,6 +3,13 @@ local builtin = require('telescope.builtin')
 
 require('telescope').setup({
 	defaults = {
+		layout_strategy = 'cursor',
+		layout_config = {
+			cursor = {
+				prompt_position = 'top',
+				width = 0.95,
+			},
+		},
 		mappings = {
 			i = {
 				['<C-j>'] = actions.move_selection_next,
