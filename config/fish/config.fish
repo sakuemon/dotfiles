c3b4750cb73aa927a8f4c8210d773a3da991885c
Tsuyoshi MIYAMOTO <sakuemon@gmail.com>
2025-05-19 18:34:53 +0900

    add path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,7 @@ set -x XDG_CONFIG_HOME $HOME/.config
 
 set LANG ja_JP.UTF-8
 
+fish_add_path /usr/local/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/go/bin
 fish_add_path /usr/local/opt/coreutils/libexec/gnubin
